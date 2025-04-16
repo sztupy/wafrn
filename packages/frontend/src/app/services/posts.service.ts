d39c5d0516c47ea6c19b631af0016537345fadf5
Zsolt Sz. Sz. Raven <mail@sztupy.hu>
2025-04-16 18:05:54 +0000

    Add support for Two Factor login
    Adds support for TOTP based MFA login. Support is preliminary, as it doesn't adhere to best practices (reset password will also reset MFA), but it adds the options to fix this later, and obviously it's better than not having this feature.
    
    Main implementation details:
    
    1. MFA Details are stored in a separate table linked to the User object. This includes the relevant details to store in a generic enough way to later support alternative providers (email, bypass codes, device, etc).
    2. MFA step is handled by sending a semi-valid JWT, which doesn't contain a `userId`, but includes information that the first factor was passed.
    3. Only after the second factor is validated will the JWT be upgraded to one which contains the `userId` which the code checks for a fully valid login
    
    There is support for setting up multiple providers (although all of them are TOTP/Authenticator App type only), so people could swap their phone by adding a new one first before removing the old one.
    
    The main issue with the current setup is that it is not considered good practive to allow a user to reset their MFA, but lack of other alternatives (like backup codes or an admin interface where admins can manually reset this) this should be okay.
    
    Roadmap for future improvements:
    1. Add support for bypass codes, and automatic bypass code setup once you add your first TOTP MFA
    2. Add support for managing User's MFA details through the admin interface
    3. Add config options so the 'MFA reset on Password Reset' can be enabled/disabled based on what the instance operator wants
    
    This should cover the main security bits and make sure this implementation is up to current standards
    
    Afterwards:
    
    4. Add support for alternative providers:
    4/A. Email might be a good candidate (although it's not really considered secure but some instance providers might enable it for convenience)
    4/B. Passkey support would definitely be nice as well
    4/C. And finally we might want to use the Wafrn-RN app as well
    5. A bit nicer UI, but as I'm mostly a backend dev this will likely remain a dream :D

--- packages/frontend/src/app/services/posts.service.ts
+++ packages/frontend/src/app/services/posts.service.ts
@@ -303,8 +303,8 @@ export class PostsService {
     const polls = elem ? unlinked.polls.filter((poll) => poll.postId === elem.id) : []
     const medias = elem
       ? unlinked.medias.filter((media) => {
-          return media.postId === elem.id
-        })
+        return media.postId === elem.id
+      })
       : []
     if (userEmojis && userEmojis.length && user && user.name) {
       userEmojis.forEach((usrEmoji) => {
@@ -316,26 +316,26 @@ export class PostsService {
     }
     const mentionedUsers = elem
       ? unlinked.mentions
-          .filter((mention) => mention.post === elem.id)
-          .map((mention) => unlinked.users.find((usr) => usr.id === mention.userMentioned))
-          .filter((mention) => mention !== undefined)
+        .filter((mention) => mention.post === elem.id)
+        .map((mention) => unlinked.users.find((usr) => usr.id === mention.userMentioned))
+        .filter((mention) => mention !== undefined)
       : []
     let emojiReactions: PostEmojiReaction[] = elem
       ? unlinked.emojiRelations.postEmojiReactions.filter((emoji) => emoji.postId === elem.id)
       : []
     const likesAsEmojiReactions: PostEmojiReaction[] = elem
       ? unlinked.likes
-          .filter((like) => like.postId === elem.id)
-          .map((likeUserId) => {
-            return {
-              emojiId: 'Like',
-              postId: elem.id,
-              userId: likeUserId.userId,
-              content: '♥️',
-              //emoji?: Emoji;
-              user: unlinked.users.find((usr) => usr.id === likeUserId.userId)
-            }
-          })
+        .filter((like) => like.postId === elem.id)
+        .map((likeUserId) => {
+          return {
+            emojiId: 'Like',
+            postId: elem.id,
+            userId: likeUserId.userId,
+            content: '♥️',
+            //emoji?: Emoji;
+            user: unlinked.users.find((usr) => usr.id === likeUserId.userId)
+          }
+        })
       : []
     emojiReactions = emojiReactions.map((react) => {
       return {
@@ -350,8 +350,8 @@ export class PostsService {
     const links = parsedAsHTML.getElementsByTagName('a')
     const quotes = elem
       ? unlinked.quotes
-          .filter((quote) => quote.quoterPostId === elem.id)
-          .map((quote) => this.processedQuotes.find((pst) => pst.id === quote.quotedPostId) as ProcessedPost)
+        .filter((quote) => quote.quoterPostId === elem.id)
+        .map((quote) => this.processedQuotes.find((pst) => pst.id === quote.quotedPostId) as ProcessedPost)
       : []
     Array.from(links).forEach((link, index) => {
       const youtubeMatch = Array.from(link.href.matchAll(this.youtubeRegex))
@@ -496,8 +496,8 @@ export class PostsService {
     const mentionRemoteUrls = post.mentionPost ? post.mentionPost?.map((elem) => elem.url) : []
     const mentionedHosts = post.mentionPost
       ? post.mentionPost?.map(
-          (elem) => this.getURL(elem.remoteId ? elem.remoteId : 'https://adomainthatdoesnotexist.google.com').hostname
-        )
+        (elem) => this.getURL(elem.remoteId ? elem.remoteId : 'https://adomainthatdoesnotexist.google.com').hostname
+      )
       : []
     const hostUrl = this.getURL(EnvironmentService.environment.frontUrl).hostname
     Array.from(links).forEach((link) => {
@@ -505,10 +505,9 @@ export class PostsService {
       if (link.innerText === link.href && youtubeMatch) {
         // NOTE: Since this should not be part of the image Viewer, we have to add then no-viewer class to be checked for later
         Array.from(youtubeMatch).forEach((youtubeString) => {
-          link.innerHTML = `<div class="watermark"><!-- Watermark container --><div class="watermark__inner"><!-- The watermark --><div class="watermark__body"><img alt="youtube logo" class="yt-watermark no-viewer" loading="lazy" src="/assets/img/youtube_logo.png"></div></div><img class="yt-thumbnail" src="${
-            EnvironmentService.environment.externalCacheurl +
+          link.innerHTML = `<div class="watermark"><!-- Watermark container --><div class="watermark__inner"><!-- The watermark --><div class="watermark__body"><img alt="youtube logo" class="yt-watermark no-viewer" loading="lazy" src="/assets/img/youtube_logo.png"></div></div><img class="yt-thumbnail" src="${EnvironmentService.environment.externalCacheurl +
             encodeURIComponent(`https://img.youtube.com/vi/${youtubeString[6]}/hqdefault.jpg`)
-          }" loading="lazy" alt="Thumbnail for video"></div>`
+            }" loading="lazy" alt="Thumbnail for video"></div>`
         })
       }
       // replace mentioned users with wafrn version of profile.
@@ -674,12 +673,11 @@ export class PostsService {
   }
 
   emojiToHtml(emoji: Emoji): string {
-    return `<img class="post-emoji" src="${
-      EnvironmentService.environment.externalCacheurl +
+    return `<img class="post-emoji" src="${EnvironmentService.environment.externalCacheurl +
       (emoji.external
         ? encodeURIComponent(emoji.url)
         : encodeURIComponent(EnvironmentService.environment.baseMediaUrl + emoji.url))
-    }">`
+      }">`
   }
 
   postContainsBlockedOrMuted(post: ProcessedPost[], isDashboard: boolean) {
