259151b8ea7b3871c54156b7cb022f7e995f9bee
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2025-08-17 16:11:39 +0200

    Disabled cache on get muted posts

--- packages/backend/utils/cacheGetters/getMutedPosts.ts
+++ packages/backend/utils/cacheGetters/getMutedPosts.ts
@@ -5,7 +5,7 @@ import { logger } from '../logger.js'
 
 async function getMutedPosts(userId: string, superMute = false): Promise<Array<string>> {
   let res: string[] = []
-  const cacheResult = await redisCache.get((superMute ? 'superMutedPosts:' : 'mutedPosts:') + userId)
+  const cacheResult = undefined //await redisCache.get((superMute ? 'superMutedPosts:' : 'mutedPosts:') + userId)
   if (cacheResult) {
     res = JSON.parse(cacheResult)
   } else {
@@ -15,17 +15,17 @@ async function getMutedPosts(userId: string, superMute = false): Promise<Array<s
         superMuted: superMute
           ? true
           : {
-            [Op.in]: [true, false, null, undefined] as any // yeah ok this is a bit dirty haha but its only one code path
-          }
+              [Op.in]: [true, false, null, undefined] as any // yeah ok this is a bit dirty haha but its only one code path
+            }
       },
       attributes: ['postId']
     })
     res = mutedPostsQuery.map((elem: any) => elem.postId)
     if (superMute && res.length) {
       const muted = res.map((elem) => "'" + (elem ? elem : '00000000-0000-0000-0000-000000000000') + "'")
-      const mutedPosts = await sequelize.query(
+      const mutedPosts = (await sequelize.query(
         `SELECT "postsId" FROM "postsancestors" where "ancestorId" IN (${muted})`
-      ) as PostAncestor[][]
+      )) as PostAncestor[][]
       res = mutedPosts[0].map((elem) => elem.postsId)
     }
     await redisCache.set((superMute ? 'superMutedPosts:' : 'mutedPosts:') + userId, JSON.stringify(res), 'EX', 600)
@@ -34,12 +34,12 @@ async function getMutedPosts(userId: string, superMute = false): Promise<Array<s
 }
 
 async function getMutedPostsMultiple(userIds: string[], superMute = false) {
-  let cacheResults: (string | null) [] = []
+  let cacheResults: (string | null)[] = []
   try {
     cacheResults = await redisCache.mget(
       userIds.map((userId) => (superMute ? 'superMutedPosts:' : 'mutedPosts:') + userId)
     )
-  } catch(error) {
+  } catch (error) {
     logger.error({
       message: `Error getMutedPostsMultiple`,
       userIds,
@@ -77,12 +77,12 @@ async function getMutedPostsMultiple(userIds: string[], superMute = false) {
       postIds.set(userId, JSON.parse(result) as string[])
     } else {
       let mutedIds = mutedFirstIds.filter((elem) => elem.userId === userId).map((elem) => elem.postId)
-      
+
       if (superMute && mutedIds.length) {
         const formattedIds = mutedIds.map((elem) => "'" + elem + "'").join(',')
-        const mutedPosts = await sequelize.query(
-          `SELECT "postsId" FROM "postsancestors" where "ancestorId" IN (${formattedIds})`,
-        ) as [PostAncestor[], unknown]
+        const mutedPosts = (await sequelize.query(
+          `SELECT "postsId" FROM "postsancestors" where "ancestorId" IN (${formattedIds})`
+        )) as [PostAncestor[], unknown]
         mutedIds = mutedPosts[0].map((elem) => elem.postsId)
       }
 
