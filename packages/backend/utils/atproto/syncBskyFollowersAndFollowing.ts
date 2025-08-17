39758ee66025ff941e1bf85a04f2cc32f90424ff
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2025-08-17 17:52:57 +0200

    Use findorcreate

--- packages/backend/utils/atproto/syncBskyFollowersAndFollowing.ts
+++ packages/backend/utils/atproto/syncBskyFollowersAndFollowing.ts
@@ -15,98 +15,56 @@ async function syncBskyFollowersAndFollowing(userId: string) {
 
     await forcePopulateUsers(followersDids, await getAdminUser())
 
-    const userFollowersExisting = (
-      await Follows.findAll({
-        include: [
-          {
-            model: User,
-            as: 'follower',
-            where: {
-              bskyDid: {
-                [Op.in]: followersDids
-              }
-            },
-            required: true
-          }
-        ],
-        where: {
-          followedId: userId
+    const followers = await User.findAll({
+      where: {
+        bskyDid: {
+          [Op.in]: followersDids
         }
-      })
-    ).map((elem) => elem.followerId)
+      }
+    })
 
-    const newFollowers = (
-      await User.findAll({
+    for await (const follower of followers) {
+      await Follows.findOrCreate({
         where: {
-          bskyDid: {
-            [Op.in]: followersDids
-          },
-          id: {
-            [Op.notIn]: userFollowersExisting
-          }
-        }
-      })
-    ).map((elem) => elem.id)
-
-    const newFollowersCreated = await Follows.bulkCreate(
-      newFollowers.map((elem) => {
-        return {
-          followerId: elem,
+          followedId: userId,
+          followerId: follower.id
+        },
+        defaults: {
+          followerId: follower.id,
           followedId: userId,
           muteQuotes: false,
           muteRewoots: false,
           accepted: true
         }
       })
-    )
+    }
 
     const followingResponse = await agent.getFollows({ actor: user.bskyDid })
     const followingdids = followingResponse.data.follows.map((elem) => elem.did)
     await forcePopulateUsers(followingdids, await getAdminUser())
-    const followingExisting = (
-      await Follows.findAll({
-        include: [
-          {
-            model: User,
-            as: 'followed',
-            where: {
-              bskyDid: {
-                [Op.in]: followersDids
-              }
-            },
-            required: true
-          }
-        ],
+    const newFollowsToCreate = await User.findAll({
+      where: {
+        bskyDid: {
+          [Op.in]: followingdids
+        }
+      }
+    })
+
+    for await (const newFollow of newFollowsToCreate) {
+      await Follows.findOrCreate({
         where: {
+          followedId: newFollow.id,
           followerId: userId
-        }
-      })
-    ).map((elem) => elem.followerId)
-
-    const newFollowing = (
-      await User.findAll({
-        where: {
-          bskyDid: {
-            [Op.in]: followingdids
-          },
-          id: {
-            [Op.notIn]: followingExisting
-          }
-        }
-      })
-    ).map((elem) => elem.id)
-
-    const newFollowingCreated = await Follows.bulkCreate(
-      newFollowers.map((elem) => {
-        return {
-          followedId: elem,
+        },
+        defaults: {
+          followedId: newFollow.id,
           followerId: userId,
           muteQuotes: false,
           muteRewoots: false,
           accepted: true
         }
       })
-    )
+    }
   }
 }
 
