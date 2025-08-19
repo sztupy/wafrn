7adc55fa1e88f14e2af6ec77f63b0e2365ce4fda
FireIsGood <fireisadequate@gmail.com>
2025-08-19 05:42:43 -0700

    feat: keyboard shortcut rebinding

--- packages/frontend/src/app/components/hotkey-manager/hotkey-manager.component.ts
+++ packages/frontend/src/app/components/hotkey-manager/hotkey-manager.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core'
+import { KeyValuePipe } from '@angular/common'
+import { Component, inject, signal, WritableSignal } from '@angular/core'
 import { MatButtonModule } from '@angular/material/button'
-import { MatDialog } from '@angular/material/dialog'
+import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog'
 import { MatTooltipModule } from '@angular/material/tooltip'
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
-import { faKeyboard } from '@fortawesome/free-solid-svg-icons'
-import { TranslateModule } from '@ngx-translate/core'
+import { faKeyboard, faRotateLeft } from '@fortawesome/free-solid-svg-icons'
+import { TranslateModule, TranslateService, _ } from '@ngx-translate/core'
+import { filter, fromEvent, Subject, takeUntil } from 'rxjs'
 import { EditorService } from 'src/app/services/editor.service'
 import { CallbackDictionary, GlobalKeydownService } from 'src/app/services/global-keydown.service'
 import { JwtService } from 'src/app/services/jwt.service'
+
+type HotkeyConfig = Record<string, string | undefined>
+type ShortcutFunctionMap = Record<string, Function>
+
+const defaultKeybinds: HotkeyConfig = {
+  scrollDown: 'j',
+  scrollUp: 'k',
+  openEditor: 'e',
+  viewKeyboardShortcuts: '?'
+}
+
+// const example_mapping: HotkeyConfig = {
+//   scrollDown: 'a',
+//   scrollUp: 'b'
+//   // openEditor: 'c',
+//   // viewKeyboardShortcuts: 'd'
+// }
+
+const example_mapping: HotkeyConfig = defaultKeybinds
+
+let hotkeysEnabled = true
 
 @Component({
   selector: 'app-hotkey-manager',
@@ -20,12 +43,16 @@ export class HotkeyManagerComponent {
 
   keyboardIcon = faKeyboard
 
-  shortcutList: CallbackDictionary = {
-    j: () => this.scrollDown(),
-    k: () => this.scrollUp(),
-    e: () => this.openEditor(),
-    '?': () => this.openHotkeyListDialog()
+  // Loaded and mapped from user profile
+  shortcutListLookup: ShortcutFunctionMap = {
+    scrollDown: () => this.scrollDown(),
+    scrollUp: () => this.scrollUp(),
+    openEditor: () => this.openEditor(),
+    viewKeyboardShortcuts: () => this.openHotkeyListDialog(),
+    no_op: () => {}
   }
+  userMapping: HotkeyConfig
+  shortcutList: WritableSignal<CallbackDictionary>
 
   constructor(
     private keyboardService: GlobalKeydownService,
@@ -33,15 +60,35 @@ export class HotkeyManagerComponent {
     private jwtService: JwtService,
     private dialogService: MatDialog
   ) {
-    this.keyboardService.keydownEvents.subscribe((key) => {
-      this.keyboardService.handleKeydown(key, this.shortcutList)
+    this.userMapping = example_mapping
+    this.shortcutList = signal(this.mapHotkeys(this.userMapping))
+
+    this.keyboardService.keydownEvents.pipe(filter(() => hotkeysEnabled)).subscribe((key) => {
+      this.keyboardService.handleKeydown(key, this.shortcutList())
     })
+  }
+
+  mapHotkeys(hotkeys: HotkeyConfig) {
+    console.log(hotkeys)
+    return Object.fromEntries(
+      Object.entries(hotkeys).map(([key, shortcutMapping]) => [
+        shortcutMapping,
+        this.shortcutListLookup[key] || this.shortcutListLookup['no_op']
+      ])
+    )
   }
 
   openHotkeyListDialog() {
     this.dialogService.closeAll()
-    this.dialogService.open(HotkeyListComponent, {
-      width: '400px'
+    const dialogRef = this.dialogService.open(HotkeyListComponent, {
+      width: '400px',
+      data: {
+        currentHotkeys: this.userMapping
+      },
+      closePredicate: () => hotkeysEnabled
+    })
+    dialogRef.afterClosed().subscribe(() => {
+      this.shortcutList.set(this.mapHotkeys(example_mapping))
     })
   }
 
@@ -64,10 +111,77 @@ export class HotkeyManagerComponent {
   }
 }
 
+interface DialogData {
+  currentHotkeys: HotkeyConfig
+}
+
 @Component({
   selector: 'app-hotkey-list-dialog',
-  imports: [TranslateModule],
+  imports: [MatButtonModule, FontAwesomeModule, TranslateModule, KeyValuePipe],
   templateUrl: './hotkey-list-dialog.component.html',
   styleUrl: './hotkey-manager.component.scss'
 })
-export class HotkeyListComponent {}
+export class HotkeyListComponent {
+  readonly data = inject<DialogData>(MAT_DIALOG_DATA)
+  mapping = this.data.currentHotkeys
+  defaultKeybinds = defaultKeybinds
+
+  changingKey: string | null = null
+  cancelSetKeybind = new Subject()
+
+  undoIcon = faRotateLeft
+
+  constructor(private translateService: TranslateService) {
+    this.cancelSetKeybind.subscribe(() => {
+      hotkeysEnabled = true
+      this.changingKey = null
+    })
+  }
+
+  getKeybind(id: string) {
+    const key = this.mapping[id] || this.translateService.instant(_('keyboard-shortcuts.unbound'))
+
+    if (this.changingKey === id) {
+      return `>${key}<`
+    }
+
+    return key
+  }
+
+  isOverlapping(key: string | undefined) {
+    if (key === undefined) return false
+
+    const allKeys = Object.entries(this.mapping).map((v) => v[1])
+    return allKeys.indexOf(key) !== allKeys.lastIndexOf(key)
+  }
+
+  setKeybind(id: string) {
+    if (!hotkeysEnabled) return
+    hotkeysEnabled = false
+    this.changingKey = id
+    const modifierKeys = ['Shift', 'Alt', 'Control']
+
+    fromEvent(document, 'keydown')
+      .pipe(takeUntil(this.cancelSetKeybind))
+      .subscribe((eventRaw) => {
+        const e = <KeyboardEvent>eventRaw
+        e.preventDefault()
+        const keyIsModifier = modifierKeys.includes(e.key)
+        if (keyIsModifier) return
+
+        const isLetter = e.code.slice(0, 3) === 'Key'
+        if (isLetter) {
+          this.mapping[id] = e.shiftKey ? e.key.toUpperCase() : e.key
+        } else {
+          this.mapping[id] = undefined
+        }
+
+        this.cancelSetKeybind.next('')
+      })
+  }
+
+  resetKeybind(id: string) {
+    this.mapping[id] = defaultKeybinds[id]
+    this.cancelSetKeybind.next('')
+  }
+}
