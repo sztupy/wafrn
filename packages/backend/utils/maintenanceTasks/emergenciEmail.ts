edf5696f370ccacd14df76386779b774fb1a7c60
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2025-08-19 17:15:44 +0200

    proofreaded by verbina

--- packages/backend/utils/maintenanceTasks/emergenciEmail.ts
+++ packages/backend/utils/maintenanceTasks/emergenciEmail.ts
@@ -28,14 +28,15 @@ async function sendMail() {
       ? '<p>We are also sorry for emailing you even you have disabled mail notifications. This is a security incident (even tho most probably nothing has actualy happened) and as such we consider it important enough to inform you about it</p>'
       : ''
 
-    const subject = `Sorry for this email, but there has been a POSSIBLE security issue on our side. We dont think it has happened tho.`
+    const subject = `Wafrn: Potential security issue, now mitigated`
     const body = `
-    <h1>There has been a possible security incident</h1>
-    <p>We do not think anything has happened. But for transparency, we are emailing all wafrn users</p>
-    <p>We would write a long email, but I rather send you a thread with the discussion and possible questions you may have regarding on the incident: <a href="https://app.wafrn.net/fediverse/post/59afc694-8136-4059-a68f-a1e602d41085">https://app.wafrn.net/fediverse/post/59afc694-8136-4059-a68f-a1e602d41085</a>
-    <p>The short version is this</p>
-    <p>We've discovered that an API endpoint had insufficient access control which may have lead to sensitive user information leaking, as a result we've published updates for wafrn. We don't have sufficient evidence to determine if this was accessed in the wild, but we believe access was unlikely.</p>
-    <p>Feel free to reply to the thread or this email with any question you have</p>
+
+    <h1>A potential security issue with Wafrn was discovered on August 18th 2025.</h1>
+    <p>We do not believe that this was utilized by bad actors, but for transparency, we are emailing all Wafrn users.</p>
+    <p>The short version is this:</p>
+    <p>We discovered that an API endpoint had insufficient access control, which may have lead to sensitive user information, such as emails and hashed passwords, leaking. The issue has been mitigated in a Wafrn update, and is no longer present on the main instance nor other instances. We don't have sufficient evidence to determine if this was used by bad actors, but we believe it to be unlikely.</p>
+    <p>A Wafrn thread regarding the issue with additional detail is here: <a href="https://app.wafrn.net/fediverse/post/59afc694-8136-4059-a68f-a1e602d41085">https://app.wafrn.net/fediverse/post/59afc694-8136-4059-a68f-a1e602d41085</a>
+    <p>Feel free to reply to the thread or this email with any questions you may have.</p>
     ${sorryForMailing}
 
 <p>If you no longer desire to get these emails, please <a href="${completeEnvironment.frontendUrl}/api/disableEmailNotifications/${user.id}/${user.activationCode}">click here</a>.</p>
