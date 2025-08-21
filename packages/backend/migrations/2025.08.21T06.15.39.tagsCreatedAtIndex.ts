e903df1334e0bf53f180c649a9f237264a394606
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2025-08-21 17:01:26 +0200

    this index works better

--- packages/backend/migrations/2025.08.21T06.15.39.tagsCreatedAtIndex.ts
+++ packages/backend/migrations/2025.08.21T06.15.39.tagsCreatedAtIndex.ts
@@ -4,11 +4,9 @@ import { Migration } from '../migrate.js'
 export const up: Migration = async (params) => {
   const queryInterface = params.context
   await queryInterface.sequelize.query(`CREATE EXTENSION IF NOT EXISTS btree_gin`)
-  await queryInterface.sequelize.query(
-    `CREATE INDEX "tags_index_createdAt_desc" ON "postTags" USING gin ("createdAt", "tagName" gin_trgm_ops);`
-  )
+  await queryInterface.sequelize.query(`CREATE INDEX "tags_created_at" ON "postTags" ("createdAt" DESC);`)
 }
 export const down: Migration = async (params) => {
   const queryInterface = params.context
-  await queryInterface.sequelize.query(`DROP INDEX "tags_index_createdAt_desc"`)
+  await queryInterface.sequelize.query(`DROP INDEX "tags_created_at"`)
 }
