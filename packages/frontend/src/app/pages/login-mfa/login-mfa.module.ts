dea756b0a002f49dd00a4deb5d5315780f34a09b
FireIsGood <fireisadequate@gmail.com>
2025-08-28 18:50:48 -0700

    fix: allow mfa logins for other accounts

--- packages/frontend/src/app/pages/login-mfa/login-mfa.module.ts
+++ packages/frontend/src/app/pages/login-mfa/login-mfa.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common'
 import { LoginMfaComponent } from './login-mfa.component'
 import { RouterModule, Routes } from '@angular/router'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
-import { userLoggedGuard } from 'src/app/guards/user-logged.guard'
 import { MatCardModule } from '@angular/material/card'
 import { MatButtonModule } from '@angular/material/button'
 import { MatFormFieldModule } from '@angular/material/form-field'
@@ -15,8 +14,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
 const routes: Routes = [
   {
     path: '',
-    component: LoginMfaComponent,
-    canActivate: [userLoggedGuard]
+    component: LoginMfaComponent
   }
 ]
 
