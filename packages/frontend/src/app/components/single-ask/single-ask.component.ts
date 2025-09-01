e294efd320ec70cb483366bc61e7896cb0947ebe
FireIsGood <fireisadequate@gmail.com>
2025-08-31 18:51:46 -0700

    feat: new ask style

--- packages/frontend/src/app/components/single-ask/single-ask.component.ts
+++ packages/frontend/src/app/components/single-ask/single-ask.component.ts
@@ -1,14 +1,14 @@
-
-import { Component, Input } from '@angular/core'
+import { Component, input, Input } from '@angular/core'
 import { AvatarSmallComponent } from '../avatar-small/avatar-small.component'
 import { Ask } from '../../interfaces/ask'
+import { RouterModule } from '@angular/router'
 
 @Component({
   selector: 'app-single-ask',
-  imports: [AvatarSmallComponent],
+  imports: [AvatarSmallComponent, RouterModule],
   templateUrl: './single-ask.component.html',
   styleUrl: './single-ask.component.scss'
 })
 export class SingleAskComponent {
-  @Input() ask!: Ask
+  ask = input.required<Ask>()
 }
