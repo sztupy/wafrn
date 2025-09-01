5963ddf1e7612336b30d353ad5da0e279d3bdbea
FireIsGood <fireisadequate@gmail.com>
2025-09-01 16:26:14 -0700

    feat: scroll by post

--- packages/frontend/src/app/components/post-list/post-list.component.ts
+++ packages/frontend/src/app/components/post-list/post-list.component.ts
@@ -1,8 +1,9 @@
-import { Component, computed, ElementRef, input, output, viewChild } from '@angular/core'
+import { afterRenderEffect, Component, computed, ElementRef, input, output, signal, viewChild } from '@angular/core'
 import { ProcessedPost } from 'src/app/interfaces/processed-post'
 import { PostModule } from '../post/post.module'
 import { LoaderComponent } from '../loader/loader.component'
 import { HotkeyService, HotkeyType } from 'src/app/services/hotkey.service'
+import { fromEvent, take } from 'rxjs'
 
 @Component({
   selector: 'app-post-list',
@@ -19,11 +20,37 @@ export class PostListComponent {
   bottomPageElementRef = viewChild<ElementRef<HTMLElement>>('bottom')
   bottomPageElement = computed(() => this.bottomPageElementRef()?.nativeElement)
   bottomPageObserver: IntersectionObserver | undefined
+  currentPostObserver: IntersectionObserver
 
-  selectedPost: number | null = null // null means no hotkeys pressed yet
+  hasScrolled = signal<boolean>(false)
+  highlightPost = signal<boolean>(false)
+
+  selectedPost: number = 0
 
   constructor(hotkeyService: HotkeyService) {
     hotkeyService.hotkeySubscription.subscribe((type) => this.handleHotkeys(type))
+    this.currentPostObserver = new IntersectionObserver((entries) => this.handleCurrentPostObserver(entries[0]))
+
+    // Observe the first post after loading
+    afterRenderEffect(() => {
+      if (this.loading()) return
+      const firstPost = this.postElementAt(0)
+      if (firstPost) {
+        this.currentPostObserver.observe(firstPost)
+      }
+    })
+
+    // If the user scrolls manually we don't pick first post on Next Post key
+    fromEvent(document, 'scroll')
+      .pipe(take(1))
+      .subscribe(() => {
+        this.hasScrolled.set(true)
+      })
+
+    // Stop highlighting if the user manually scrolls with the mouse
+    fromEvent(document, 'wheel').subscribe(() => {
+      this.highlightPost.set(false)
+    })
   }
 
   ngOnInit() {
@@ -36,6 +63,27 @@ export class PostListComponent {
       this.loadPosts.emit()
     })
     this.bottomPageObserver.observe(this.bottomPageElement()!)
+  }
+
+  handleCurrentPostObserver(entry: IntersectionObserverEntry) {
+    if (entry.isIntersecting) return
+    const before = this.selectedPost
+
+    // Observe next post
+    const scrolledBelowPost = entry.boundingClientRect.top < 0
+    if (scrolledBelowPost) {
+      if (this.selectedPost < this.posts().length - 1) {
+        this.selectedPost += 1
+      }
+    } else {
+      if (this.selectedPost > 0) {
+        this.selectedPost -= 1
+      }
+    }
+    // Ignore non-moving observations
+    if (this.selectedPost === before) return
+
+    this.observeSelectedPost()
   }
 
   postElementAt(index: number): HTMLElement | null {
@@ -58,11 +106,6 @@ export class PostListComponent {
   }
 
   previousPost() {
-    if (this.selectedPost === null) {
-      this.selectedPost = 0
-      this.scrollToSelectedPost()
-      return
-    }
     if (this.selectedPost > 0) {
       this.selectedPost -= 1
     }
@@ -70,14 +113,16 @@ export class PostListComponent {
   }
 
   nextPost() {
-    if (this.selectedPost === null) {
-      this.selectedPost = 0
+    // Has not scrolled so we use the first post
+    if (!this.hasScrolled()) {
       this.scrollToSelectedPost()
       return
     }
+
     if (this.selectedPost < this.posts().length - 1) {
       this.selectedPost += 1
     }
+    // Next post on the last post triggers loading new posts and scrolls to the loader
     if (this.selectedPost === this.posts().length - 1) {
       this.loadPosts.emit()
       this.scrollToLoader()
@@ -88,15 +133,26 @@ export class PostListComponent {
 
   scrollToLoader() {
     this.bottomPageElement()?.scrollIntoView()
+    this.afterPostScroll()
   }
 
   scrollToSelectedPost() {
     const nextPost = this.postElementAt(this.selectedPost ?? 0)
     if (nextPost === null) return
     nextPost?.scrollIntoView({ behavior: 'instant' })
+    this.afterPostScroll()
   }
 
-  test() {
-    console.log('hi')
+  afterPostScroll() {
+    this.observeSelectedPost()
+    this.highlightPost.set(true)
+  }
+
+  observeSelectedPost() {
+    this.currentPostObserver.disconnect()
+    const postElem = this.postElementAt(this.selectedPost)
+    if (postElem) {
+      this.currentPostObserver.observe(postElem)
+    }
   }
 }
