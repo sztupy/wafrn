3c56523131bc08a2a594f331bf5d20476f151870
HermitCyclop <hermitcyclop@zaclys.net>
2025-09-07 22:14:05 +0200

    add french trans lation in the settings drop down menu

--- packages/frontend/src/app/app.component.ts
+++ packages/frontend/src/app/app.component.ts
@@ -36,7 +36,7 @@ export class AppComponent implements OnInit {
     this.title = this.titleService.getTitle()
     GlobalData.appDefaultTitle = this.title
 
-    this.translateService.addLangs(['en', 'pl', 'es'])
+    this.translateService.addLangs(['en', 'pl', 'es', 'fr'])
     this.translateService.setDefaultLang('en')
 
     // User specified language
