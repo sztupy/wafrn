39c27fe77bd546e0acf8cd4ed91c61afba0e13c4
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2025-09-08 22:44:06 +0200

    better index

--- packages/backend/migrations/2025.09.08T16.33.55.mediaUrlIndex.ts
+++ packages/backend/migrations/2025.09.08T16.33.55.mediaUrlIndex.ts
@@ -3,7 +3,7 @@ import { Migration } from '../migrate.js'
 
 export const up: Migration = async (params) => {
   const queryInterface = params.context
-  await queryInterface.sequelize.query(`CREATE INDEX "mediaUrlIndex" ON "medias" ("url");`)
+  await queryInterface.sequelize.query(`CREATE INDEX "mediaUrlIndex" ON "medias" USING gin ("url" gin_trgm_ops);`)
 }
 export const down: Migration = async (params) => {
   const queryInterface = params.context
