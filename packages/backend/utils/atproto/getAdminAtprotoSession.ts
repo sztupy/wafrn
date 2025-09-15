66983656f2a0bbf1fbe6d5cd7bcb000e9bf00d3a
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2025-09-15 19:39:36 +0200

    make session last one hour only

--- packages/backend/utils/atproto/getAdminAtprotoSession.ts
+++ packages/backend/utils/atproto/getAdminAtprotoSession.ts
@@ -1,12 +1,17 @@
 import { AtpAgent } from '@atproto/api'
 import { getAtProtoSession } from '../../atproto/utils/getAtProtoSession.js'
 import { getAdminUser } from '../getAdminAndDeletedUser.js'
+import { logger } from '../logger.js'
 
 let adminSession: AtpAgent | undefined
+let lastTimeSessionCreated = new Date(0)
 
 async function getAdminAtprotoSession(): Promise<AtpAgent> {
-  if (!adminSession) {
+  const currentDate = new Date()
+  if (!adminSession || currentDate.getTime() - lastTimeSessionCreated.getTime() >= 3600000) {
     adminSession = await getAtProtoSession(await getAdminUser(), true)
+    lastTimeSessionCreated = new Date()
+    logger.info('Force getting new admin bsky session')
   }
   return adminSession
 }
