f3883008e688bda8d7b72c7872d6d1408783f1d2
FireIsGood <fireisadequate@gmail.com>
2025-09-19 09:57:20 -0700

    feat: try nothing at all

--- packages/backend/routes/frontend.ts
+++ packages/backend/routes/frontend.ts
@@ -619,8 +619,7 @@ function formatIndexSeo(indexRaw: string, options: MetaTagOptions) {
       ...[
         { property: 'og:video', content: urlSanitized },
         { property: 'twitter:player:stream', content: urlSanitized },
-        { property: 'twitter:player:stream:content_type', content: typeSanitized },
-        { property: 'twitter:card', content: 'summary_large_image' }
+        { property: 'twitter:player:stream:content_type', content: typeSanitized }
       ]
     )
     // Width and height separate
