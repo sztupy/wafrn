e1d101a5e43f3fc2ed26c1c6bf90c144a2a4f0b8
FireIsGood <fireisadequate@gmail.com>
2025-09-19 09:18:01 -0700

    feat: basic text formatting on quotes

--- packages/backend/routes/frontend.ts
+++ packages/backend/routes/frontend.ts
@@ -380,7 +380,7 @@ async function getPostSEOCache(id: string): Promise<MetaTagOptions & { content?:
         const nameSanitized = sanitizeStringForSEO(quotedPost.user.name).substring(0, 65)
         const urlSanitized = sanitizeStringForSEO(quotedPost.user.url).substring(0, 65)
         const contentSanitized = sanitizeStringForSEO(quotedPost.content)
-        quotedPostContent = `\n\nQuoting ${nameSanitized} (${urlSanitized}${quotedPost.user.isRemoteUser ? '' : '@' + completeEnvironment.instanceUrl}))\n\n${contentSanitized}`
+        quotedPostContent = `\n\n> Quoting ${nameSanitized} (${urlSanitized}${quotedPost.user.isRemoteUser ? '' : '@' + completeEnvironment.instanceUrl}))\n>\n> ${contentSanitized}`
       }
     }
 
