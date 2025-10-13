d7aabe14d15e5086cb2ce4d506dec064f713f6f7
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2025-10-13 21:32:44 +0200

    lets invalidate cache of lang files

--- packages/frontend/src/app/app.config.ts
+++ packages/frontend/src/app/app.config.ts
@@ -5,7 +5,7 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader'
 import { HttpClient } from '@angular/common/http'
 
 const httpLoaderFactory: (http: HttpClient) => TranslateHttpLoader = (http: HttpClient) =>
-  new TranslateHttpLoader(http, './assets/i18n/', '.json')
+  new TranslateHttpLoader(http, './assets/i18n/', `.json?date=${new Date().getTime()}`)
 
 export const appConfig: ApplicationConfig = {
   providers: [
